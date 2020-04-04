3996f55278cdeb1a875a24bd786eec22977712c6
Tarun Batra <tarun1998in@live.com>
2020-04-04 15:02:33 -0400

    adding function to open old site

--- dragging.js
+++ dragging.js
@@ -2,3 +2,7 @@ function opengithub() {
   console.log('clicked');
   window.open("https://www.github.com");
 }
+function openoldsite() {
+  console.log('clicked');
+  window.open("indexold.html");
+}
