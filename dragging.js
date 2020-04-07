3cdc9b9bf55ce8dc344db1d153a5d703492a5dda
Tarun Batra <tarun1998in@live.com>
2020-04-07 19:18:19 -0400

    real time time

--- dragging.js
+++ dragging.js
@@ -14,5 +14,18 @@ function terminate() {
   console.log('working')
   document.write('<script type="text/undefined">')
 }
-var dt = new Date();
-document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
+function startTime() {
+  var today = new Date();
+  var h = today.getHours() - 12;
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
--- pong.py
+++ pong.py
@@ -19,6 +19,7 @@ paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5,stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350,0)
+paddle_a.tilt(20)
 # Paddle B
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
